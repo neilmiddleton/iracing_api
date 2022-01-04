628ba4de2d658f75db8bb4a3109795181552fc15
Neil Middleton <neil.middleton@zendesk.com>
2022-01-04 12:56:18 +0000

    First commit

---
+++ lib/iracing_api.rb
@@ -0,0 +1,8 @@
+# frozen_string_literal: true
+
+require_relative "iracing_api/version"
+
+module IracingApi
+  class Error < StandardError; end
+  # Your code goes here...
+end
---
+++ lib/iracing_api/version.rb
@@ -0,0 +1,5 @@
+# frozen_string_literal: true
+
+module IracingApi
+  VERSION = "0.1.0"
+end
